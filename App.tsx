81d1950a062959106b460ae9349d2d3885060f24
FloridaLangara <106621772+FloridaLangara@users.noreply.github.com>
2023-02-07 12:52:44 -0800

    feat: Add React Navigation Stack

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from 'react-native-screens/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import SplashScreen from './src/screens/SplashScreen';
 import HomeScreen from './src/screens/HomeScreen';

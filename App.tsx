e64052bc920b82062541766bb38b902fba3a2237
FloridaLangara <106621772+FloridaLangara@users.noreply.github.com>
2023-02-08 19:43:09 -0800

    feat: Add logic to differentiate if user logged in.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {firebase} from '@react-native-firebase/auth';
 
 import SplashScreen from './src/screens/SplashScreen';
 import HomeScreen from './src/screens/HomeScreen';
@@ -8,14 +9,36 @@ import HomeScreen from './src/screens/HomeScreen';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={'Splash'}>
-        <Stack.Screen name={'Splash'} component={SplashScreen} />
-        <Stack.Screen name={'Home'} component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+  }, []);
+
+  if (!isLoggedIn) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={'Splash'}>
+          <Stack.Screen name={'Splash'} component={SplashScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={'Splash'}>
+          <Stack.Screen name={'Splash'} component={SplashScreen} />
+          <Stack.Screen name={'Home'} component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 };
 
 export default App;

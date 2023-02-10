dd4aa2e1cf503ac845e21123a77376eccb233dc0
FloridaLangara <106621772+FloridaLangara@users.noreply.github.com>
2023-02-09 20:57:27 -0800

    feat: Add configuration for Google sign in

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {firebase} from '@react-native-firebase/auth';
 
 import SplashScreen from './src/screens/SplashScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,16 +13,21 @@ const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
         setIsLoggedIn(true);
       } else {
         setIsLoggedIn(false);
       }
     });
+
+    GoogleSignin.configure({
+      webClientId:
+        '259021060250-a486v22la0hut46k5f0r3rntoevh9unt.apps.googleusercontent.com',
+    });
   }, []);
 
-  if (!isLoggedIn) {
+  if (isLoggedIn) {
     return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName={'Splash'}>

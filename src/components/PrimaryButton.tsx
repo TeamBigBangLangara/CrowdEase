f6ae992547a20ce24c6ff7a5d5d444e79124a351
rojintag <106949675+rojintag@users.noreply.github.com>
2023-02-18 02:05:59 -0800

    feat: create the PrimaryButton component

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,51 @@
+import { Pressable, Text, GestureResponderEvent, StyleSheet } from 'react-native';
+
+const PrimaryButton = (props: { onPress: ((event: GestureResponderEvent) => void) ; title: string; }) => {
+  return (
+  <Pressable
+  onPress={props.onPress}
+  style={({ pressed }) => [        
+    pressed ? styles.pressedShadow : styles.shadowProp,
+    styles.button 
+       ]}
+    >
+      <Text style= {styles.text}>{props.title}</Text>
+</Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#B687F4',
+    borderRadius: 22,
+    width: 330,
+    height: 42
+  },
+  pressedShadow : {
+    shadowColor: 'rgba(131, 53, 253, 0.9)',
+    shadowOffset: {
+      width: 12,
+      height: 8},
+    shadowOpacity: 1,
+    shadowRadius: 13,
+  }, 
+  shadowProp: {
+    shadowColor: 'rgba(131, 53, 253, 0.9)',
+    shadowOffset: {
+      width: 1,
+      height: 1
+    },
+    shadowOpacity: 1,
+    shadowRadius: 3,
+
+  },
+  text: {
+    color: '#212121',
+    fontSize: 14,
+  }
+})
+
+export default PrimaryButton;

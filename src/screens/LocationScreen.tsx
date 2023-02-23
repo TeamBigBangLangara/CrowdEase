4446d489dc0d0136bbf5b119af1e340202cc1d27
LOPESWI3 <104600109+LOPESWI3@users.noreply.github.com>
2023-02-23 01:51:13 -0800

    feat: get location from postal code

--- src/screens/LocationScreen.tsx
+++ src/screens/LocationScreen.tsx
@@ -1,7 +1,19 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button, Switch, Platform, PermissionsAndroid } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Switch,
+  Platform,
+  PermissionsAndroid,
+  Alert,
+  TextInput
+} from 'react-native'
 import Geolocation from 'react-native-geolocation-service'
+import { getPostalCodeCoordinates } from '../services/postalCodeApi'
 import { AuthStackNavigationProps } from '../types/types'
+import { isPostalCodeValid } from '../utils/postalCode'
 
 interface GeoPositionState {
   latitude: number | null
@@ -11,15 +23,32 @@ interface GeoPositionState {
 
 const LocationScreen = ({ navigation }: AuthStackNavigationProps<'LocationScreen'>) => {
   const [isEnabled, setIsEnabled] = useState(false)
+  const [postalCode, setPostalCode] = useState('')
   const [location, setLocation] = useState<GeoPositionState>({
     latitude: null,
     longitude: null,
     error: null
   })
 
-  const getLocation = () => {
+  const getLocation = async () => {
     if (signUpValidation()) {
-      navigation.navigate('BottomTabs')
+      if (isEnabled) {
+        //navigation.navigate('BottomTabs')
+      } else if (isPostalCodeValid(postalCode)) {
+        try {
+          let results = await getPostalCodeCoordinates(postalCode)
+          setLocation({
+            latitude: results?.latt,
+            longitude: results?.longt,
+            error: null
+          })
+        } catch (error) {
+          Alert.alert('Error', `${error}`)
+        }
+        //navigation.navigate('BottomTabs')
+      } else {
+        Alert.alert('Error', 'Enter a valid Postal Code to proceed')
+      }
     }
   }
 
@@ -29,7 +58,6 @@ const LocationScreen = ({ navigation }: AuthStackNavigationProps<'LocationScreen
 
   const toggleSwitchPress = async () => {
     setIsEnabled(previousState => !previousState)
-    console.log(isEnabled)
     await requestPermissions()
   }
 
@@ -37,10 +65,12 @@ const LocationScreen = ({ navigation }: AuthStackNavigationProps<'LocationScreen
     if (Platform.OS === 'ios') {
       const auth = await Geolocation.requestAuthorization('whenInUse')
       if (auth === 'granted') {
-        console.log('IOS')
         await getGeolocationCoordinates()
       } else {
-        console.log('Permission denied')
+        Alert.alert(
+          'Error',
+          'Location permission Denied. Enter Location or provide permission in settings'
+        )
       }
     }
 
@@ -48,9 +78,17 @@ const LocationScreen = ({ navigation }: AuthStackNavigationProps<'LocationScreen
       if (await requestLocationPermissionAndroid()) {
         await getGeolocationCoordinates()
       } else {
-        console.log('Permission denied')
+        Alert.alert(
+          'Error',
+          'Location permission Denied. Enter Location or provide permission in settings'
+        )
       }
     }
+  }
+
+  const setPostalCodeText = (inputText: string) => {
+    const filteredText = inputText.replace(/[\s-]+/g, '')
+    setPostalCode(filteredText)
   }
 
   const requestLocationPermissionAndroid = async (): Promise<boolean> => {
@@ -65,12 +103,9 @@ const LocationScreen = ({ navigation }: AuthStackNavigationProps<'LocationScreen
           buttonPositive: 'OK'
         }
       )
-      console.log('granted', granted)
       if (granted === 'granted') {
-        console.log('You can use Geolocation')
         return true
       } else {
-        console.log('You cannot use Geolocation')
         return false
       }
     } catch (err) {
@@ -81,7 +116,6 @@ const LocationScreen = ({ navigation }: AuthStackNavigationProps<'LocationScreen
   const getGeolocationCoordinates = async () => {
     return Geolocation.getCurrentPosition(
       position => {
-        console.log(position)
         setLocation({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
@@ -89,8 +123,7 @@ const LocationScreen = ({ navigation }: AuthStackNavigationProps<'LocationScreen
         })
       },
       error => {
-        // See error code charts below.
-        console.log(error.code, error.message)
+        Alert.alert('Error', `${error.message}`)
       },
       { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
     )
@@ -99,15 +132,38 @@ const LocationScreen = ({ navigation }: AuthStackNavigationProps<'LocationScreen
   return (
     <View style={styles.wrapper}>
       <Text>CrowdEase works best with location permission </Text>
-      <View style={styles.container}>
-        <Switch
-          hitSlop={{ top: 20, left: 20, right: 20, bottom: 20 }}
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitchPress}
-          value={isEnabled}
-        />
+      <Text>
+        Location is used to improve your experience by detecting events and crowds around you.
+      </Text>
+      <View style={styles.outerTopContainer}>
+        <Text style={styles.container}>Turn on GPS</Text>
+        <View style={styles.container}>
+          <Switch
+            hitSlop={{ top: 20, left: 1, right: 1, bottom: 20 }}
+            trackColor={{ false: '#767577', true: '#81b0ff' }}
+            thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitchPress}
+            value={isEnabled}
+          />
+        </View>
+      </View>
+      <View style={styles.outerBottomContainer}>
+        <View style={styles.dottedLine}>
+          <View style={{ flex: 1, height: 1, backgroundColor: 'black', margin: 20 }} />
+          <View>
+            <Text style={{ width: 50, textAlign: 'center' }}>Or</Text>
+          </View>
+          <View style={{ flex: 1, height: 1, backgroundColor: 'black', margin: 20 }} />
+        </View>
+        <View>
+          <TextInput
+            style={styles.textInputStyle}
+            onChangeText={setPostalCodeText}
+            value={postalCode}
+            placeholder="Enter Postal Code Manually"
+          />
+        </View>
       </View>
       <View style={styles.buttonStyle}>
         <Button onPress={getLocation} title="Get Location" accessibilityLabel="Get Location" />
@@ -118,6 +174,7 @@ const LocationScreen = ({ navigation }: AuthStackNavigationProps<'LocationScreen
 
 const styles = StyleSheet.create({
   wrapper: {
+    flex: 1,
     backgroundColor: 'lightblue',
     alignItems: 'center',
     margin: 20
@@ -130,11 +187,46 @@ const styles = StyleSheet.create({
     padding: 2
   },
   container: {
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: 5,
+    margin: 5
+  },
+  outerTopContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     padding: 2,
-    margin: 4
+    width: '100%',
+    height: '30%',
+    margin: 5
+  },
+  outerBottomContainer: {
+    flex: 1,
+    alignItems: 'center',
+    paddingtop: 2,
+    width: '100%',
+    height: '30%',
+    margintop: 20
+  },
+  dottedLine: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 2,
+    width: '100%',
+    height: '50%',
+    margin: 5
+  },
+  textInputStyle: {
+    height: 40,
+    width: 333,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'lightgrey'
   },
   iconStyle: {
     gap: 5,

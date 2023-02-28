cc7d55def7892fc017f984a3e2e5071a21763795
Diego Santa Cruz <dsantacruzmeoo00@mylangara.ca>
2023-02-28 06:38:00 -0800

    BB-87 Components with basic styling

--- src/components/PassedEventCard.tsx
+++ src/components/PassedEventCard.tsx
@@ -2,32 +2,54 @@ import React from 'react'
 import { View, Image, Text, StyleSheet } from 'react-native'
 
 import BookMarked from '../components/BookMarked'
+import IconText from './IconText'
 
-const PlaceNameHere = (props: {
-  eventTime: string
+const PassedEventCard = (props: {
+  eventDate: string
   eventName: string
-  eventlocation: string
+  eventLocation: string
   eventParticipantsQty: string
 }) => {
   return (
-    <View>
-      <Image source={require('@expo/snack-static/react-native-logo.png')} />
-      <View>
-        <Text>{props.eventTime}</Text>
-        <Text>{props.eventName}</Text>
-        {/* Icon <Image source={require('@expo/snack-static/react-native-logo.png')} /> */}
-        <Text>{props.eventlocation}</Text>
-        <Text>{props.eventParticipantsQty}</Text>
+    <View style={styles.generalWrapper}>
+      <Text style={styles.text}>{props.eventDate}</Text>
+      <Text style={styles.text}>{props.eventName}</Text>
+      <View style={styles.locationParticipantsWrapper}>
+        <IconText icon={require('../assets/Pin.png')} text={`${props.eventLocation}`} />
+        <IconText
+          icon={require('../assets/Participants.png')}
+          text={`${props.eventParticipantsQty} participants`}
+        />
       </View>
-      <BookMarked clicked={true} />
     </View>
   )
 }
 
 //Style
 const styles = StyleSheet.create({
-    
-
+  generalWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
+    borderRadius: 22,
+    borderWidth: 1,
+    backgroundColor: 'rgba(12, 12, 14, 0.5)',
+    paddingHorizontal: 20,
+    paddingVertical: 20
+  },
+  text: {
+    color: '#FAFBFC',
+    fontSize: 14,
+    lineHeight: 18
+  },
+  locationParticipantsWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
 })
 
-export default PlaceNameHere
+export default PassedEventCard
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,29 +1,60 @@
 import React, { useState } from 'react'
-import { TextInput, StyleSheet } from 'react-native'
+import { TextInput, StyleSheet, Image, View } from 'react-native'
+
+import IconButton from './IconButton'
 
 const SearchForm = (props: { searchEvent: () => {}; placeHolder: string }) => {
   const [searchValue, setSearchValue] = useState('')
 
   return (
-    <TextInput
-      onChangeText={setSearchValue}
-      value={searchValue}
-      onSubmitEditing={props.searchEvent}
-      placeholder={props.placeHolder}
-      style={styles.input}
-    />
+    <View style={styles.componentWrapper}>
+      <View style={styles.searchWrapper}>
+        <Image source={require('../assets/search.png')} />
+        <TextInput
+          onChangeText={setSearchValue}
+          value={searchValue}
+          onSubmitEditing={props.searchEvent}
+          placeholder={props.placeHolder}
+          style={styles.input}
+          placeholderTextColor="#FFFFFF"
+        />
+        <Image source={require('../assets/mic.png')} />
+      </View>
+      <IconButton iconPath={require('../assets/categories_icon.png')} style={styles.iconButton} />
+    </View>
   )
 }
 
 //Style
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
+  componentWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    height: 42,
+    borderRadius: 22,
+    gap: 20
+  },
+  searchWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
     borderWidth: 2,
-    padding: 10,
+    borderColor: '#938F99',
+    paddingHorizontal: 16,
+    height: 42,
     borderRadius: 20
-  }
+  },
+  input: {
+    flex: 1,
+    marginHorizontal: 12,
+    paddingHorizontal: 10,
+    borderColor: '#938F99',
+    color: '#FFFFFF'
+  },
+  iconButton: {}
 })
 
 export default SearchForm

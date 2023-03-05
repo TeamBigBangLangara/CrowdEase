f2538af982fde20ace1c7d15a4f72d8829ae621a
floridajoca <florida.joca@gmail.com>
2023-03-05 09:13:19 -0800

    fix: Minor

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Pressable, Image, StyleSheet } from 'react-native'
 
-const IconButton = (props: any) => {
+const IconButton = (props: { onClick: () => void; iconPath: any }) => {
   return (
-    <Pressable
-      onPress={props.onClick}
-      style={[styles.button]}>
+    <Pressable onPress={props.onClick} style={styles.button}>
       <Image source={props.iconPath} />
     </Pressable>
   )
@@ -13,8 +11,7 @@ const IconButton = (props: any) => {
 
 const styles = StyleSheet.create({
   button: {
-    display: 'flex',
-    alignItems: 'center'
+    display: 'flex'
   }
 })
 

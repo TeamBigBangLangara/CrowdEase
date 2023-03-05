7390f0b97b2b4d6acbf626824750747a8109b2ef
floridajoca <florida.joca@gmail.com>
2023-03-05 14:11:23 -0800

    fix: Rollback changes

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
-import SearchForm from '../components/SearchForm';
-
-import EventCard from '../components/EventCard';
-import IconButton from '../components/IconButton';
-import RateCard from '../components/RateCard';
 
 const SplashScreen = ({ navigation, }: { navigation: any }) => {
-  const test = () => {
-    console.log(test);
-  };
 
   return (
     <View style={{ flex: 1, }}>
@@ -20,27 +12,6 @@ const SplashScreen = ({ navigation, }: { navigation: any }) => {
       <Pressable onPress={() => navigation.navigate('Sign Up')}>
         <Text>Sign up</Text>
       </Pressable>
-      <Text>PAST EVENT</Text>
-      <EventCard
-        eventType={'past'}
-        eventTime={'11am'}
-        eventDate={'11 march'}
-        eventName={'test'}
-        eventLocation={'tjfgsg'}
-        eventParticipantsQty={8555}
-        onBookmarkPress={test}
-      />
-      <View style={{ marginBottom: 10, }}></View>
-      <EventCard
-        eventType={'NowEvent'}
-        eventTime={'11am'}
-        eventName={'test'}
-        eventLocation={'tjfgsg'}
-        eventParticipantsQty={8555}
-        onBookmarkPress={test}
-      />
-
-      <RateCard onSubmitPress={test} />
     </View>
   );
 };

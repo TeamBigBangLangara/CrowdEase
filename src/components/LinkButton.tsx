30760f1f316cf9c6b2979e34b24e2b7b59f3a5fa
floridajoca <florida.joca@gmail.com>
2023-03-05 14:09:53 -0800

    fix: Remove unnecessary styling

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -12,11 +12,8 @@ const styles = StyleSheet.create({
   text: {
     color: '#90EE90',
     fontSize: 14,
-    marginHorizontal: 10,
-    width: 60,
     borderBottomColor: '#90EE90',
     borderBottomWidth: 1,
-    justifyContent: 'center',
   },
 });
 

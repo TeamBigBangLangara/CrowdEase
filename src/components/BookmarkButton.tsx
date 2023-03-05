2c9d7e75d119335b97edd521542bf10a50bf437e
floridajoca <florida.joca@gmail.com>
2023-03-04 17:18:43 -0800

    fix: Add on click handler for bookmark button

---
+++ src/components/BookmarkButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Image, Pressable } from 'react-native'
+
+const BookmarkButton = (props: { onBookmarkPress: () => void }) => {
+  return (
+    <Pressable onPress={props.onBookmarkPress}>
+      <Image source={require('../assets/bookmark.png')} />
+    </Pressable>
+  )
+}
+
+export default BookmarkButton

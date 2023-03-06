d8f4bfa9cbcba3e6c9208ca20300553f8a74653f
floridajoca <florida.joca@gmail.com>
2023-03-05 20:27:16 -0800

    feat: Create DropdownButton component

---
+++ src/components/DropdownButton.tsx
@@ -0,0 +1,27 @@
+import { Image, Pressable, StyleSheet, Text } from "react-native";
+import React from "react";
+import { colors } from "../styles/colors";
+import { fontSize } from "../styles/fonts";
+
+const DropdownButton = (props: {onDropdownPress: () => void, label: string}) => {
+  return (
+    <Pressable onPress={props.onDropdownPress} style={styles.container}>
+      <Text style={styles.label}>{props.label}</Text>
+      <Image source={require('../assets/downIcon.png')} />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginTop: 8,
+    gap: 4,
+  },
+  label: {
+    color: colors.netural.backgroundWhite,
+    fontSize: fontSize.body,
+    lineHeight: 18,
+  },
+});
+export default DropdownButton;

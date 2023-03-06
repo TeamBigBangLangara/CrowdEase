b23216434720e8084a9bf4bc71715d5b14a7eaec
floridajoca <florida.joca@gmail.com>
2023-03-05 17:39:50 -0800

    fix: Fix eslint issues

--- src/types/navigationTypes.ts
+++ src/types/navigationTypes.ts
@@ -1,7 +1,7 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { TabParams } from '../App'
-import { MainStackParams } from '~/App'
-import { MaterialBottomTabNavigationProp } from '@react-navigation/material-bottom-tabs'
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { TabParams } from '../App';
+import { MainStackParams } from '../App';
+import { MaterialBottomTabNavigationProp } from '@react-navigation/material-bottom-tabs';
 
 // TODO: use official typing when fixed, see https://github.com/react-navigation/react-navigation/issues/10802#issuecomment-1326687295
 export type MainStackNavigationProps<T extends keyof MainStackParams> = NativeStackScreenProps<

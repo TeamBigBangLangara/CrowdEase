36da688bd990a90a83d5e3ccfb1778cf72d90a62
Diego Santa Cruz <dsantacruzmeoo00@mylangara.ca>
2023-03-07 14:24:35 -0800

    report card styling

--- src/components/ReportCard.tsx
+++ src/components/ReportCard.tsx
@@ -1,17 +1,20 @@
 import { View, StyleSheet, Text } from 'react-native';
 
+import { colors } from "../styles/colors";
+import { fontSize, fontFamily, fontWeightSubtitle } from "../styles/fonts";
+
 const ReportCard = (props: { date: string; eventNumber: number; participantsQty: number }) => {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.infoContainer}>
         <Text style={styles.header}>Date</Text>
         <Text style={styles.info}>{props.date}</Text>
       </View>
-      <View>
+      <View style={styles.infoContainer}>
         <Text style={styles.header}>Event Number</Text>
         <Text style={styles.info}>{props.eventNumber}</Text>
       </View>
-      <View>
+      <View style={styles.infoContainer}>
         <Text style={styles.header}>Event Participants</Text>
         <Text style={styles.info}>{props.participantsQty}</Text>
       </View>
@@ -21,24 +24,28 @@ const ReportCard = (props: { date: string; eventNumber: number; participantsQty:
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#121214',
-    borderRadius: 12,
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingVertical: 10,
-    paddingHorizontal: 6,
-    marginHorizontal: 12,
+    backgroundColor: colors.netural.surfaceBlack,
+    justifyContent: 'space-between',
+    borderRadius: 22,
+    paddingVertical: 16,
+    paddingHorizontal: 18,
+  },
+  infoContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   header: {
-    fontWeight: '500',
-    fontSize: 12,
-    color: '#E6E1E5',
+    fontWeight: fontWeightSubtitle,
+    fontSize: fontSize.caption,
+    color: colors.netural.surfaceWhite,
     textAlign: 'center',
   },
   info: {
     fontWeight: '500',
-    fontSize: 16,
+    fontSize: fontSize.subtitle2,
     color: '#E6E1E5',
   },
 });

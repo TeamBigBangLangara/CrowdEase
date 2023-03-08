ccbabffe5ef0d6124deee15fafb01d3d8bf8fa01
rojintag <106949675+rojintag@users.noreply.github.com>
2023-03-07 22:32:27 -0800

    style: style LinkButton component

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -4,21 +4,27 @@ import { fontFamily, fontSize, fontWeightBody} from "../styles/fonts";
 const LinkButton = (props: { 
   onPress: (event: GestureResponderEvent) => void 
   label: string 
-  color: string }) => {
+  style: {
+    color: string,
+    borderBottomColor: string
+  } }) => {
   return (
-    <Pressable onPress={props.onPress}>
-      <Text style={[styles.text, {color: props.color, borderBottomColor: props.color}]}>{props.label}</Text>
+    <Pressable onPress={props.onPress} style={styles.textContainer}>
+      <Text style={[styles.text, props.style]}>{props.label}</Text>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
+  textContainer: {
+    alignSelf: 'flex-end'
+  },
   text: {
     fontSize: fontSize.caption,
     fontFamily: fontFamily.body,
     fontWeight: fontWeightBody,
     borderBottomWidth: 1,
-  },
+  }
 });
 
 export default LinkButton;

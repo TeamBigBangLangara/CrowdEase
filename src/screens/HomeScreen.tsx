9b02652c26a7c640f7774a4cf4aae1d68f671fe6
floridajoca <florida.joca@gmail.com>
2023-03-07 22:16:02 -0800

    Feature: remove map from home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
 
 import { signOut } from '../auth/user';
 import { MainStackNavigationProps } from '../types/navigationTypes';
@@ -12,23 +11,14 @@ const HomeScreen = ({ navigation, }: MainStackNavigationProps<'HomeScreen'>) => 
       <Pressable onPress={signOut}>
         <Text>Sign out</Text>
       </Pressable>
-      <MapView
-        style={{height: 100, width: 100,}}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   wrapper: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 

b505769aae07dc122ee605391a1c7bd01f6c2b2a
rojintag <106949675+rojintag@users.noreply.github.com>
2023-03-07 22:48:17 -0800

    style: style PrimaryButton component

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,16 +1,30 @@
-import { Pressable, Text, GestureResponderEvent, StyleSheet } from 'react-native';
+import { Pressable, Text, GestureResponderEvent, StyleSheet, View } from 'react-native';
+import InsetShadow from 'react-native-inset-shadow'
 
-const PrimaryButton = (props: { onPress: ((event: GestureResponderEvent) => void) ; label: string; }) => {
+import { colors } from '../styles/colors'
+import { fontFamily } from '../styles/fonts'
+import { fontSize } from '../styles/fonts'
+import { fontWeightSubtitle2 } from '../styles/fonts'
+
+const PrimaryButton = (props: { onPress: ((event: GestureResponderEvent) => void); label: string; }) => {
+
   return (
-  <Pressable
-  onPress={props.onPress}
-  style={({ pressed, }) => [
-    pressed ? styles.pressedShadow : styles.shadowProp,
-    styles.button
-       ]}
-    >
-      <Text style= {styles.text}>{props.label}</Text>
-</Pressable>
+    <View style={[styles.button, styles.elevation]}>
+      <InsetShadow containerStyle={styles.shadow}
+        shadowRadius={5}
+        shadowOffset={10}
+        shadowOpacity={0.8}>
+      <Pressable
+          onPress={props.onPress}
+          style={({ pressed, }) => [
+            pressed ? styles.pressedShadow : styles.shadowProp,
+            styles.button
+          ]}
+        >
+          <Text style={styles.text}>{props.label}</Text>
+        </Pressable>
+      </InsetShadow>
+    </View>
   );
 };
 
@@ -19,33 +33,39 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#B687F4',
+    backgroundColor: colors.primary.primaryPurpleDark,
     borderRadius: 22,
     width: 330,
     height: 42,
   },
-  pressedShadow : {
+  pressedShadow: {
     shadowColor: 'rgba(131, 53, 253, 0.9)',
     shadowOffset: {
       width: 12,
-      height: 8,},
+      height: 8,
+    },
     shadowOpacity: 1,
     shadowRadius: 13,
   },
-  shadowProp: {
-    shadowColor: 'rgba(131, 53, 253, 0.9)',
-    shadowOffset: {
-      width: 1,
-      height: 1,
-    },
-    shadowOpacity: 1,
-    shadowRadius: 3,
-
+  shadowProp: {  
+    shadowOffset: {width: -2, height: 6},  
+    shadowColor: 'rgba(131, 53, 253, 1)',  
+    shadowOpacity: 0.9,  
+    shadowRadius: 3,  
+  },
+  elevation: {  
+    shadowColor:  'rgba(131, 53, 253, .7)',  
+    elevation: 20,  
   },
   text: {
-    color: '#212121',
-    fontSize: 14,
+    color: colors.netural.surfaceBlack,
+    fontSize: fontSize.body,
+    fontFamily: fontFamily.body,
+    fontWeight: fontWeightSubtitle2
   },
+  shadow: {
+    borderRadius: 22
+  }
 });
 
 export default PrimaryButton;

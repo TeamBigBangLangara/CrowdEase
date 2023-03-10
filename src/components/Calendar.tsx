549ab9f2924b376fd0d00e6e6b72d8d17cbdad2b
Diego Santa Cruz <dsantacruzmeoo00@mylangara.ca>
2023-03-10 12:05:25 -0800

    style: general calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,15 +1,32 @@
 import React, { useCallback, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Calendar, CalendarProvider, WeekCalendar, CalendarList } from "react-native-calendars";
+import { Calendar, CalendarProvider, WeekCalendar, CalendarList, ExpandableCalendar } from "react-native-calendars";
 
 
 const CalendarComponent = () => {
-
-
   return (
-      <CalendarProvider style={{backgroundColor: 'black',}} date={new Date().toISOString() } >
-        <WeekCalendar current={'2023-03-09'} onDayPress={(day) => console.log(day)} firstDay={1} pagingEnabled={false} style={{backgroundColor: 'black',}}/>
-      </CalendarProvider>
+      <CalendarProvider date={'2023-03-10'}>
+        <ExpandableCalendar
+        hideDayNames={false}
+        onDayPress={(day) => console.log(day)} 
+        firstDay={0}
+        hideKnob={true}
+        closeOnDayPress={false}
+        hideExtraDays={true}
+        showSixWeeks={true}
+        calendarHeight={200}
+        calendarWidth={380}
+        theme={{
+          backgroundColor: '#121214',
+          calendarBackground: '#121214',
+          selectedDayTextColor: "#90EE90",
+          selectedDayBackgroundColor: '#121214',
+          arrowColor: "#E6E1E5",
+          monthTextColor: "#90EE90",
+        }}
+        enableSwipeMonths={false}
+        /> 
+      </CalendarProvider>  
   );
 
   };

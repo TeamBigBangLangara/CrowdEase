7af32fbf96abcf3555ae5d2deba83a9a650378a3
rojintag <106949675+rojintag@users.noreply.github.com>
2023-03-10 02:30:23 -0800

    style: style the navigationBottom component

--- src/components/navigation/NavigationBottomTab.tsx
+++ src/components/navigation/NavigationBottomTab.tsx
@@ -1,62 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../../screens/HomeScreen';
 import MapScreen from '../../screens/MapScreen';
 import EventScreen from '../../screens/EventScreen';
 import WeekManager from '../../screens/WeekManagerScreen';
+import { colors } from '../../styles/colors';
+import { fontFamily, fontWeightSubtitle2, fontSize } from '../../styles/fonts'
+import LinearGradient from 'react-native-linear-gradient';
 
 const Tab = createBottomTabNavigator();
+
 const NavigationBottomTab = () => {
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { backgroundColor: colors.netural.surfaceBlack },
+        tabBarActiveTintColor: colors.primary.primaryPurpleDark,
+      }}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarIcon: () => (
-            <View>
-              <Text>Home</Text>
-            </View>
-          ),
+          tabBarIcon: ({ focused }) => (
+            <Image source={focused ? require('../../assets/icons/navIcons/HomeActive.png') : require('../../assets/icons/navIcons/Home.png')} />
+          )
         }}
       />
       <Tab.Screen
         name="Map"
         component={MapScreen}
         options={{
-          tabBarIcon: () => (
-            <View>
-              <Text>Map</Text>
-            </View>
-          ),
+          tabBarIcon: ({ focused }) => (
+              <Image source={focused ? require('../../assets/icons/navIcons/MapActive.png') : require('../../assets/icons/navIcons/Map.png')} />
+          )
         }}
       />
       <Tab.Screen
         name="Report"
         component={WeekManager}
         options={{
-          tabBarIcon: () => (
-            <View>
-              <Text>Report</Text>
-            </View>
-          ),
+          tabBarIcon: ({ focused }) => (
+            <Image source={focused ? require('../../assets/icons/navIcons/ReportActive.png') : require('../../assets/icons/navIcons/Report.png')} />
+          )
         }}
       />
       <Tab.Screen
         name="Events"
         component={EventScreen}
         options={{
-          tabBarIcon: () => (
-            <View>
-              <Text>Events</Text>
-            </View>
-          ),
+          tabBarIcon: ({ focused }) => (
+            <Image source={focused ? require('../../assets/icons/navIcons/EventActive.png') : require('../../assets/icons/navIcons/Event.png')} />
+          )
         }}
       />
     </Tab.Navigator>
   );
 };
+
 
 export default NavigationBottomTab;

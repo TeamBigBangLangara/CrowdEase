e02167b5699ec88b6bbf9e25f55f5d45421a696f
floridajoca <florida.joca@gmail.com>
2023-03-10 00:15:00 -0800

    Fix: Modify Calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,20 +1,23 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Calendar, CalendarProvider, WeekCalendar, CalendarList } from "react-native-calendars";
 
-
-const CalendarComponent = () => {
-
+const CalendarComponent = (props: {onDayPress: (date: Date) => void}) => {
 
   return (
+    <View style={styles.calendar}>
       <CalendarProvider style={{backgroundColor: 'black',}} date={new Date().toISOString() } >
-        <WeekCalendar current={'2023-03-09'} onDayPress={(day) => console.log(day)} firstDay={1} pagingEnabled={false} style={{backgroundColor: 'black',}}/>
+        <WeekCalendar current={'2023-04-15'} onDayPress={(day) => props.onDayPress(day)} firstDay={1} pagingEnabled={false} />
       </CalendarProvider>
+    </View>
   );
-
   };
-
 //Style
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  calendar: {
+    height:55,
+    marginVertical: 24,
+  },
+});
 
 export default CalendarComponent;

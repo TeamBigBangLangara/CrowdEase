74ca2687323190a8888bba6d6eb2fbf55a692e7b
floridajoca <florida.joca@gmail.com>
2023-03-10 12:43:54 -0800

    Fix: Gradient Text container

--- src/components/navigation/NavigationBottomTab.tsx
+++ src/components/navigation/NavigationBottomTab.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, } from 'react';
+import React, { useEffect } from 'react';
 import { Image,Text, View, StyleSheet} from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -8,7 +8,7 @@ import EventScreen from '../../screens/EventScreen';
 import WeekManager from '../../screens/WeekManagerScreen';
 import { colors } from '../../styles/colors';
 import { requestUsers } from '../../api/bigBangAPI/users';
-import { fontFamily, fontWeightSubtitle2, fontSize } from '../../styles/fonts'
+import { fontFamily, fontWeightSubtitle2, fontSize } from '../../styles/fonts';
 import GradientText from '../GradientText';
 
 const Tab = createBottomTabNavigator();
@@ -28,20 +28,19 @@ useEffect(() => {
   return (
     <Tab.Navigator initialRouteName="Home"
       screenOptions={{
-        headerShown: false,
         tabBarShowLabel: false,
         headerStyle: {
           backgroundColor: colors.netural.surfaceBlack,
         },
         headerTintColor: '#fff',
-        tabBarStyle: { backgroundColor: colors.netural.surfaceBlack, height: 60, borderTopWidth: 2, borderTopColor: colors.netural.outlineGrey },
+        tabBarStyle: { backgroundColor: colors.netural.surfaceBlack, height: 60, borderTopWidth: 2, borderTopColor: colors.netural.outlineGrey, },
         tabBarActiveTintColor: colors.primary.primaryPurpleDark,
       }}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: ({ focused, }) => (
             <View style={styles.iconContainer}>
               <Image source={focused ? require('../../assets/icons/navIcons/HomeActive.png') : require('../../assets/icons/navIcons/Home.png')} />
               {focused ?
@@ -53,14 +52,14 @@ useEffect(() => {
                   style={styles.label} /> :
                 <Text style={[styles.label, styles.inactiveLabel]}>Home</Text>}
             </View>
-          )
+          ),
         }}
       />
       <Tab.Screen
         name="Map"
         component={MapScreen}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: ({ focused, }) => (
             <View style={styles.iconContainer}>
             <Image source={focused ? require('../../assets/icons/navIcons/MapActive.png') : require('../../assets/icons/navIcons/Map.png')} />
             {focused ?
@@ -71,14 +70,14 @@ useEffect(() => {
                 end={colors.primary.gradientDark.end}
                 style={styles.label} /> :
               <Text style={[styles.label, styles.inactiveLabel]}>Map</Text>}
-          </View>          )
+          </View>          ),
         }}
       />
       <Tab.Screen
         name="Report"
         component={WeekManager}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: ({ focused, }) => (
             <View style={styles.iconContainer}>
             <Image source={focused ? require('../../assets/icons/navIcons/ReportActive.png') : require('../../assets/icons/navIcons/Report.png')} />
             {focused ?
@@ -90,14 +89,14 @@ useEffect(() => {
                 style={styles.label} /> :
               <Text style={[styles.label, styles.inactiveLabel]}>Report</Text>}
           </View>
-          )
+          ),
         }}
       />
       <Tab.Screen
         name="Events"
         component={EventScreen}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: ({ focused, }) => (
             <View style={styles.iconContainer}>
             <Image source={focused ? require('../../assets/icons/navIcons/EventActive.png') : require('../../assets/icons/navIcons/Event.png')} />
             {focused ?
@@ -109,7 +108,7 @@ useEffect(() => {
                 style={styles.label} /> :
               <Text style={[styles.label, styles.inactiveLabel]}>Events</Text>}
           </View>
-          )
+          ),
         }}
       />
     </Tab.Navigator>
@@ -119,13 +118,13 @@ useEffect(() => {
 
 const styles = StyleSheet.create({
   tabbar: {
-    backgroundColor: colors.netural.surfaceBlack
+    backgroundColor: colors.netural.surfaceBlack,
   },
   iconContainer: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 13
+    marginTop: 13,
   },
   label: {
     flex: 1,
@@ -134,8 +133,8 @@ const styles = StyleSheet.create({
     fontWeight: fontWeightSubtitle2,
   },
   inactiveLabel: {
-    color: colors.netural.surfaceWhite
-  }
-})
+    color: colors.netural.surfaceWhite,
+  },
+});
 
 export default NavigationBottomTab;

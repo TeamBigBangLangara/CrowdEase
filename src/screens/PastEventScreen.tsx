b2bac1231b07244d4db66f9dfdb2cf583c809b9f
rojintag <106949675+rojintag@users.noreply.github.com>
2023-03-17 03:17:52 -0700

    feat: get user id in pastEventScreen

--- src/screens/PastEventScreen.tsx
+++ src/screens/PastEventScreen.tsx
@@ -1,25 +1,66 @@
 import RateCard from "../components/RateCard"
-import { View } from "react-native"
-import { useState } from "react";
+import { Alert, FlatList, StyleSheet, View, ScrollView, SafeAreaView } from "react-native"
+import { useQuery } from "react-query";
+import { getEvents } from "../api/event";
+
+
+import EventCard from "../components/EventCard";
+import { getUser } from '../auth/user';
+import { LoggedUser } from "types/types";
+import { useEffect, useState } from "react";
+import { getRating } from "../api/bigBangAPI/rating";
+import { colors } from "../styles/colors"
 
 const PastEventScreen = () => {
-  const [starRating, setStarRating] = useState(0);
+  const [userInfo, setUserInfo] = useState<LoggedUser>({ uid: '', email: '', });
 
-  const onStarPress = (id: number) => {
-    setStarRating(id);
-  }
-return(
-  <View>
-       <RateCard
-            onSubmitPress={() => { }}
-            onSkipPress={() => { }}
-            onStarPress={onStarPress}
-            imageActive={require("../assets/icons/StarActive.png")}
-            imageInactive={require("../assets/icons/star.png")}
-            activeStarCount={starRating}
+  const requestEvents = useQuery("events", () => getEvents(),
+    {
+      onError: (error: TypeError) => {
+        Alert.alert("Error", error.message);
+      },
+    })
+
+
+  useEffect(() => {
+    async function fetchUser() {
+      const user = await getUser();
+      setUserInfo(user);
+      console.log(user.uid);
+
+      const rating = await getRating(user.uid);
+      console.log(rating);
+
+    }
+    fetchUser();
+  }, []);
+  return (
+    <SafeAreaView style={{ flex: 1, }}>
+      <ScrollView>
+        <View style={styles.container}>
+          <FlatList
+            data={requestEvents.data}
+            renderItem={({ item, }) =>
+              <EventCard
+                key={item.id}
+                event={item}
+                eventType={"past"}
+                userID={userInfo?.uid}
+              />
+            }
           />
-  </View>
-)
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.netural.backgroundBlack,
+    paddingHorizontal: 20,
+    paddingVertical: 24,
+}
+})
 
 export default PastEventScreen

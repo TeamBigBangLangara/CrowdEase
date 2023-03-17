dba8c002057ef8578c6cbfe27339e4c4d4280156
floridajoca <florida.joca@gmail.com>
2023-03-17 14:31:30 -0700

    fix: Add markers of events and list the event card details

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,232 +1,112 @@
-import React, { useEffect, useState } from 'react';
-import {
-  Animated,
-  Dimensions,
-  Image,
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import MapView, { Heatmap, Marker } from "react-native-maps";
+import { Alert, Dimensions, StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { useQuery } from "react-query";
+import React, { useRef, useState } from "react";
+import Carousel from "react-native-snap-carousel";
 
-import { markers } from '../model/mapData';
+import EventCard from "../components/EventCard";
+import { getEvents } from "../api/event";
 import { mapDarkStyle } from "../styles/maps";
 
-const { width, height, } = Dimensions.get('window');
-const CARD_HEIGHT = 150;
-const CARD_WIDTH = width * 0.8;
-const SPACING_FOR_CARD_INSET = width * 0.1 - 10;
-
 const MapScreen = () => {
-  const initialMapState = {
-    markers,
-    region: {
-      latitude: 22.62938671242907,
-      longitude: 88.4354486029795,
-      latitudeDelta: 0.04864195044303443,
-      longitudeDelta: 0.040142817690068,
-    },
+
+  const mapRef = React.useRef<any>(null);
+  const carouselRef = useRef<Carousel<{uri: string}>>(null);
+
+  const [isCarouselVisible, setIsCarouselVisible] = useState(false);
+  const ITEM_WIDTH = Dimensions.get('screen').width * 0.8;
+
+  const requestEvents = useQuery('events', () => getEvents());
+
+  const _renderItem = ({ item, }) => {
+    return (
+      <EventCard
+        event={item}
+        eventType='actual'
+      />
+    );
   };
-  let mapIndex = 0;
-  let mapAnimation = new Animated.Value(0);
 
-  const [state, setState] = useState(initialMapState);
-  const [activeCoordinate, setActiveCoordinate] = useState({});
-  const [heatmapData, setHeatmapData] = useState([
-    { latitude: 49.2803897, longitude: -123.1126552, },
-    { latitude: 49.2797959, longitude: -123.0974419, },
-    { latitude: 49.2756562, longitude: -123.1096782, },
-    { latitude: 49.2838, longitude: -123.1193,}
-    // more data points
-  ]);
+  const renderCarousel = () => {
+    return (
+      <Carousel
+        ref={carouselRef}
+        data={requestEvents.data}
+        renderItem={_renderItem}
+        sliderWidth={Dimensions.get('screen').width}
+        itemWidth={ITEM_WIDTH}
+      />
+    );
+  };
 
-  const _map = React.useRef<any>(null);
-  const _scrollView = React.useRef<any>(null);
-
-  useEffect(() => {
-    mapAnimation.addListener(({ value, }) => {
-      let index = Math.floor(value / CARD_WIDTH + 0.3); // animate 30% away from landing on the next item
-      if (index >= state.markers.length) {
-        index = state.markers.length - 1;
-      }
-      if (index <= 0) {
-        index = 0;
-      }
-
-      const regionTimeout = setTimeout(() => {
-        if (mapIndex !== index) {
-          mapIndex = index;
-          const { coordinate, } = markers[index];
-          _map.current.animateToRegion(
-            {
-              ...coordinate,
-              latitudeDelta: state.region.latitudeDelta,
-              longitudeDelta: state.region.longitudeDelta,
-            },
-            350
-          );
-          setActiveCoordinate(coordinate);
-        }
-      }, 10);
-      // clearTimeout(regionTimeout);
+  const renderEventMarkers = () => {
+    return requestEvents.data?.map((event, index) => {
+      return (
+        <Marker
+          key={event.id}
+          coordinate={{
+            latitude: Number(event.location.latitude) ,
+            longitude: Number(event.location.longitude),
+             }}
+          pinColor={"#B687FF"}
+          onPress={() => onMarkerPress(index)}
+         ></Marker>
+      );
     });
-  });
+  };
 
-  const interpolations = markers.map((marker, index) => {
-    const inputRange: number[] = [
-      (index - 1) * CARD_WIDTH,
-      index * CARD_WIDTH,
-      (index + 1) * CARD_WIDTH
-    ];
-
-    const scale = mapAnimation.interpolate({
-      inputRange,
-      outputRange: [1, 1.5, 1],
-      extrapolate: 'clamp',
-    });
-
-    return { scale, };
-  });
-
-  // const onMarkerPress = (mapEventData) => {
-  //   const markerID = mapEventData._targetInst.return.key;
-
-  //   let x = (markerID * CARD_WIDTH) + (markerID * 20);
-  //   if (Platform.OS === 'ios') {
-  //     x = x - SPACING_FOR_CARD_INSET;
-  //   }
-
-  //   _scrollView.current.scrollTo({x: x, y: 0, animated: true});
-  // }
+  const onMarkerPress = (index) =>  {
+    setIsCarouselVisible(true);
+    setTimeout(() => {
+      carouselRef.current.snapToItem(index);
+    }, 100);
+  };
 
   return (
-    <View style={styles.container}>
+    <View>
       <MapView
-        ref={_map}
+        ref={mapRef}
         provider={"google"}
         customMapStyle={mapDarkStyle}
         style={styles.map}
         initialRegion={{
           latitude: 49.264131,
           longitude: -123.1569595,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.1523,
-        }}>
-
-        {markers.map((marker, index) => {
-          return (
-            <Marker
-              coordinate={marker.coordinate}
-              pinColor={activeCoordinate !== marker.coordinate ? "purple" : "green"}
-              key={Math.random()}></Marker>
-          );
-        })}
-        {<Marker
-            coordinate={{ latitude: 37.78825, longitude: -122.4324, }}
-            title="Marker Title"
-            description="Marker Description"
-          /> }
-        {<Heatmap points={heatmapData} radius={50}/> }
-      </MapView>
-
-      <Animated.ScrollView
-        ref={_scrollView}
-        horizontal
-        scrollEventThrottle={1}
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled
-        snapToAlignment="center"
-        snapToInterval={CARD_WIDTH + 20}
-        style={styles.scrollView}
-        contentContainerStyle={{
-          paddingHorizontal: Platform.OS === 'android' ? SPACING_FOR_CARD_INSET : 0,
+          latitudeDelta: 0.2,
+          longitudeDelta: 0.3,
         }}
-        onScroll={Animated.event(
-          [
-            {
-              nativeEvent: {
-                contentOffset: {
-                  x: mapAnimation,
-                },
-              },
-            }
-          ],
-          { useNativeDriver: true, }
-        )}>
-        {markers.map((marker, index) => (
-          <View style={styles.card} key={index}>
-            <Image source={marker.image} style={styles.cardImage} resizeMode="cover" />
-            <View style={styles.textContent}>
-              <Text numberOfLines={1} style={styles.cardDescription}>
-                {marker.timing}
-              </Text>
-              <Text numberOfLines={1} style={styles.cardTitle}>
-                {marker.title}
-              </Text>
-              <Text numberOfLines={1} style={styles.cardDescription}>
-                {marker.location}
-              </Text>
-              <Text numberOfLines={1} style={styles.cardDescription}>
-                {marker.participants}
-              </Text>
-            </View>
-          </View>
-        ))}
-      </Animated.ScrollView>
+      >
+        <Marker
+          key={1}
+          coordinate={{
+            latitude: 49.264131,
+            longitude: -123.1569595,
+          }}
+          onPress={() => Alert.alert("Home", "My location")}
+          pinColor={"#90EE90"}
+        ></Marker>
+        {renderEventMarkers()}
+      </MapView>
+      { isCarouselVisible ?
+        <View style={styles.carousel}>
+          {renderCarousel()}
+        </View>
+        : ""
+      }
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   map: {
     width: '100%',
     height: '100%',
   },
-  card: {
-    elevation: 2,
-    backgroundColor: '#FFF',
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5,
-    marginHorizontal: 10,
-    shadowColor: '#000',
-    shadowRadius: 5,
-    shadowOpacity: 0.3,
-    //shadowOffset: { x: 2, y: -2 },
-    height: CARD_HEIGHT,
-    width: CARD_WIDTH,
-    overflow: 'hidden',
-  },
-  cardImage: {
-    flex: 3,
-    width: '100%',
-    height: '100%',
-    alignSelf: 'center',
-  },
-  textContent: {
-    flex: 2,
-    padding: 10,
-  },
-  cardTitle: {
-    fontSize: 12,
-    // marginTop: 5,
-    fontWeight: 'bold',
-  },
-  scrollView: {
+  carousel: {
     position: 'absolute',
     bottom: 0,
-    left: 0,
-    right: 0,
-    paddingVertical: 10,
-  },
-  cardDescription: {
-    fontSize: 12,
-    color: '#444',
+    width: '100%',
   },
 });
 
 export default MapScreen;
-

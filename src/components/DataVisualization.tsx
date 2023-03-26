245faf28c7c3c27806fdb9abfbd04a32779377ea
rojintag <106949675+rojintag@users.noreply.github.com>
2023-03-26 03:02:26 -0700

    feat: get the participants of events for data's value

--- src/components/DataVisualization.tsx
+++ src/components/DataVisualization.tsx
@@ -1,21 +1,44 @@
 
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import { getEvents } from "../api/event";
+import { useState } from "react";
+import { Alert, StyleSheet, View } from "react-native";
+import { useQuery } from "react-query";
 import { VictoryBar, VictoryChart, VictoryAxis, VictoryLabel } from "victory-native";
 import { colors } from "../styles/colors";
 import { fontFamily, fontSize } from "../styles/fonts";
+import { getDate } from "../utils/getDate";
 
 const DataVisualization = () => {
-  const days = [
-    { day: "MON" },
-    { day: "TUE" },
-    { day: "WED" },
-    { day: "THU" },
-    { day: "FRI" },
-    { day: "SAT" },
-    { day: "SUN" },
+  const { week } = getDate();
+  
+  const [dateFilter, setDateFilter] = useState(week[0])
+  const requestEvents = useQuery("events", () => getEvents(),
+    {
+      onError: (error: TypeError) => {
+        Alert.alert("Error", error.message);
+      },
+    }
+  );
+  
+  const data = [
+    { day: "MON", value: 0 },
+    { day: "TUE", value: 0 },
+    { day: "WED", value: 0 },
+    { day: "THU", value: 0 },
+    { day: "FRI", value: 0 },
+    { day: "SAT", value: 0 },
+    { day: "SUN", value: 0 },
   ];
-
+  
+  requestEvents.data?.forEach((event) => {
+    for (let i = 0; i < 7; i++) {
+      if (event.dates.date === week[i]) {
+        data[i].value += event.participants;
+      }
+    }
+  });
+  
+  
   const barChartSvg = {
     fill: colors.netural.surfaceWhite,
   };
@@ -31,17 +54,6 @@ const DataVisualization = () => {
     },
   };
 
-  const data = [
-    { day: "MON", value: 5 },
-    { day: "TUE", value: 6 },
-    { day: "WED", value: 4 },
-    { day: "THU", value: 10 },
-    { day: "FRI", value: 8 },
-    { day: "SAT", value: 7 },
-    { day: "SUN", value: 10 },
-  ];
-
-
   return (
     <View style={styles.container}>
       <VictoryChart height={200}>
@@ -49,44 +61,44 @@ const DataVisualization = () => {
           style={axisStyles}
           tickLabelComponent={<VictoryLabel style={styles.xaxisSvg} />}
         />
-          <VictoryBar
-            data={data}
-            x="day"
-            y="value"
-            style={{ data: barChartSvg }}
-            barWidth={20}cornerRadius={{bottomLeft:(0), bottomRight:(0), topLeft:(10), topRight:(10)}}
-            events={[
-              {
-                target: 'data',
-                eventHandlers: {
-                  onPressIn: (event, data) => {
-                    return [
-                      {
-                        target: 'data',
-                        eventKey: 'all',
-                        mutation: (props: any, clickedData: { datum: { day: string, value: number } }) => {
-                          const fill = props.style?.fill;
-                          return fill === colors.netural.surfaceWhite
-                            ? colors.netural.surfaceWhite
-                            : { fill: colors.secondaryGreenDark };
-                        },
+        <VictoryBar
+          data={data}
+          x="day"
+          y="value"
+          style={{ data: barChartSvg }}
+          barWidth={20} cornerRadius={{ bottomLeft: (0), bottomRight: (0), topLeft: (10), topRight: (10) }}
+          events={[
+            {
+              target: 'data',
+              eventHandlers: {
+                onPressIn: (event, data) => {
+                  return [
+                    {
+                      target: 'data',
+                      eventKey: 'all',
+                      mutation: (props: any, clickedData: { datum: { day: string, value: number } }) => {
+                        const fill = props.style?.fill;
+                        return fill === colors.netural.surfaceWhite
+                          ? colors.netural.surfaceWhite
+                          : { fill: colors.secondaryGreenDark };
                       },
-                      {
-                        target: 'data',
-                        mutation: (props: any, clickedData: { datum: { day: string, value: number } }) => {
-                          console.log(data.datum);
-                          // setSelectedBar(data?.datum);
-                          return {
-                            style: { fill: colors.secondaryGreenDark  },
-                          };
-                        },
+                    },
+                    {
+                      target: 'data',
+                      mutation: (props: any, clickedData: { datum: { day: string, value: number } }) => {
+                        console.log(data.datum);
+                        // setSelectedBar(data?.datum);
+                        return {
+                          style: { fill: colors.secondaryGreenDark },
+                        };
                       },
-                    ];
-                  },
+                    },
+                  ];
                 },
               },
-            ]}
-          />
+            },
+          ]}
+        />
       </VictoryChart>
     </View>
   );
--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,6 +1,6 @@
 export const getDate= () => {
-  const curr = new Date(); // get current date
-  const first = curr.getDate() - curr.getDay() + 1; // First day is the day of the month - the day of the week
+  const curr = new Date() ; // get current date
+  const first = curr.getDate() - curr.getDay(); // First day is Sunday of the current week
   const last = first + 6; // last day is the first day + 6
 
   const firstDay = new Date(curr.setDate(first));
@@ -12,7 +12,20 @@ export const getDate= () => {
   const formattedLastDay = lastDay.toLocaleDateString('en-US', options);
   const today = new Date().toLocaleDateString('en-US', options);
 
-  return { formattedFirstDay, formattedLastDay, today };
+    const dayOfWeek = curr.getDay(); // Sunday is 0, Monday is 1, and so on.
+    const monday = new Date(curr);
+    monday.setDate(curr.getDate() - dayOfWeek + 1); // Subtract days to get to Monday of current week.
+  
+    const week: string[] = [];
+    for (let i = 0; i < 7; i++) {
+      const date = new Date(monday);
+      date.setDate(monday.getDate() + i);
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Add leading zero if needed.
+      const day = date.getDate().toString().padStart(2, '0'); // Add leading zero if needed.
+      const formattedDate = `${year}-${month}-${day}`;
+      week.push(formattedDate);
+    }
+
+  return { formattedFirstDay, formattedLastDay, today, week };
 };
-
-

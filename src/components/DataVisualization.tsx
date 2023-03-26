1050816fa57862b4928ddfeae1edcd697fb202fd
rojintag <106949675+rojintag@users.noreply.github.com>
2023-03-26 00:06:15 -0700

    feat: change shape and clickable bars

--- src/components/DataVisualization.tsx
+++ src/components/DataVisualization.tsx
@@ -1,60 +1,100 @@
-import { BarChart, XAxis } from "react-native-svg-charts";
+
+import React from "react";
 import { StyleSheet, View } from "react-native";
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryLabel } from "victory-native";
 import { colors } from "../styles/colors";
 import { fontFamily, fontSize } from "../styles/fonts";
 
 const DataVisualization = () => {
   const days = [
-    { label: "MON", },
-    { label: "TUE", },
-    { label: "WED", },
-    { label: "THU", },
-    { label: "FRI", },
-    { label: "SAT", },
-    { label: "SUN", }
+    { day: "MON" },
+    { day: "TUE" },
+    { day: "WED" },
+    { day: "THU" },
+    { day: "FRI" },
+    { day: "SAT" },
+    { day: "SUN" },
   ];
 
-  const textColor = {
+  const barChartSvg = {
     fill: colors.netural.surfaceWhite,
   };
 
-
-  const barChartSvg = {
-    fill: colors.secondaryGreenDark,
+  const axisStyles = {
+    axis: {
+      stroke: colors.netural.surfaceWhite,
+    },
+    tickLabels: {
+      fontSize: fontSize.caption,
+      fontFamily: fontFamily.body,
+      fill: colors.netural.surfaceWhite,
+    },
   };
 
-  const data = [5, 6, 4, 10, 8, 7, 10];
+  const data = [
+    { day: "MON", value: 5 },
+    { day: "TUE", value: 6 },
+    { day: "WED", value: 4 },
+    { day: "THU", value: 10 },
+    { day: "FRI", value: 8 },
+    { day: "SAT", value: 7 },
+    { day: "SUN", value: 10 },
+  ];
+
 
   return (
-    <View style={{ marginHorizontal: 1,}}>
-      <BarChart
-        spacingInner={0.6}
-        style={styles.barChart}
-        data={data}
-        svg={{...barChartSvg,}}
-        contentInset={{ top: 20, }}
-      >
-      </BarChart>
-      <XAxis
-        data={days}
-        formatLabel={(value: string, index:  number) => days[index].label}
-        contentInset={{ left: 15, right: 15, }}
-        style={styles.xaxis}
-        svg={{...styles.xaxisSvg, ...textColor,}}
-      />
+    <View style={styles.container}>
+      <VictoryChart height={200}>
+        <VictoryAxis
+          style={axisStyles}
+          tickLabelComponent={<VictoryLabel style={styles.xaxisSvg} />}
+        />
+          <VictoryBar
+            data={data}
+            x="day"
+            y="value"
+            style={{ data: barChartSvg }}
+            barWidth={20}cornerRadius={{bottomLeft:(0), bottomRight:(0), topLeft:(10), topRight:(10)}}
+            events={[
+              {
+                target: 'data',
+                eventHandlers: {
+                  onPressIn: (event, data) => {
+                    return [
+                      {
+                        target: 'data',
+                        eventKey: 'all',
+                        mutation: (props: any, clickedData: { datum: { day: string, value: number } }) => {
+                          const fill = props.style?.fill;
+                          return fill === colors.netural.surfaceWhite
+                            ? colors.netural.surfaceWhite
+                            : { fill: colors.secondaryGreenDark };
+                        },
+                      },
+                      {
+                        target: 'data',
+                        mutation: (props: any, clickedData: { datum: { day: string, value: number } }) => {
+                          console.log(data.datum);
+                          // setSelectedBar(data?.datum);
+                          return {
+                            style: { fill: colors.secondaryGreenDark  },
+                          };
+                        },
+                      },
+                    ];
+                  },
+                },
+              },
+            ]}
+          />
+      </VictoryChart>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  barChart: {
-    height: 166,
-    width: 357,
-    borderBottomWidth: 2,
-    borderBottomColor: colors.netural.surfaceWhite,
-  },
-  xaxis: {
-    marginTop: 2,
+  container: {
+    marginHorizontal: 1,
   },
   xaxisSvg: {
     fontSize: fontSize.caption,

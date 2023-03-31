b49cb3632c6bfa884f7285740a64e0969b2c6876
bhanuj <nagpal.bhanuj@gmail.com>
2023-03-30 21:05:36 -0700

    Date formatting

--- src/screens/WeekManagerScreen.tsx
+++ src/screens/WeekManagerScreen.tsx
@@ -6,6 +6,13 @@ import { getDate } from "../utils/getDate";
 import ReportCard from "../components/ReportCard";
 import { colors } from "../styles/colors";
 
+const daysOfWeek:string[] = ["MON","TUE","WED","THU","FRI","SAT","SUN"];
+
+const formatDate = (dateStr:string)=> {
+  let date = new Date(dateStr);
+  return date.toLocaleString('default', { month: 'short',}) + " " + date.getDate() +", "+ daysOfWeek[date.getDay()];
+};
+
 const WeekManagerScreen = () => {
   const { week, } = getDate();
   const requestEvents = useQuery("events", () => getEvents(),
@@ -27,6 +34,7 @@ const WeekManagerScreen = () => {
 
   requestEvents.data?.forEach((event) => {
     for (let i = 0; i < 7; i++) {
+      data[i].day = formatDate(week[i]);
       if (event.dates.date === week[i]) {
         data[i].value = data[i].value>0 ? data[i].value+1 : 1;
         data[i].participant += event.participants;

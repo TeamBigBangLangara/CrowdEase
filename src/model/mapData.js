208730d9d7305f4b1a4fd171438e5cff3e436c8a
rojintag <106949675+rojintag@users.noreply.github.com>
2023-04-14 06:56:26 -0700

    feat: cgange the data of mapData

--- src/model/mapData.js
+++ src/model/mapData.js
@@ -1,42 +1,27 @@
-export const heatMap =[
+export const heatMap = [
   {
-  latitude: 49.2805197,
-  longitude: -123.1007382,
-  weight: 10,
+    latitude: 49.279672,
+    longitude: -123.130293,
+    weight: 5063,
   },
-{
-  latitude: 49.030235,
-  longitude: -122.287062,
-  weight: 10,
+  {
+    latitude: 49.030235,
+    longitude: -122.287062,
+    weight: 5502,
   },
-{
-  latitude: 49.2776612,
-  longitude: -123.1081074 ,
-  weight: 10,
-  },
-{
-  latitude: 49.2776612,
-  longitude: -123.1081074,
-  weight: 1450,
+  {
+    latitude: 49.2776612,
+    longitude: -123.1081074,
+    weight: 3461,
   },
   {
     latitude: 49.2806462,
     longitude: -123.1208652,
-    weight: 400,
+    weight: 11026,
   },
   {
-    latitude: 49.279672,
-    longitude: -123.130293,
-    weight: 10,
+    latitude: 49.2776612,
+    longitude: -123.1081074,
+    weight: 11229,
   },
-  {
-    latitude: 49.2777469,
-    longitude: -123.1174817,
-    weight: 10,
-  },
-  {
-    latitude: 49.2760688,
-    longitude: -123.1127117,
-    weight: 50,
-  }
 ]

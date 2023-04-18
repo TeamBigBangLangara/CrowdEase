b265675aa6a7634b512f56ec2d183cdb20071c3e
floridajoca <florida.joca@gmail.com>
2023-04-17 22:34:56 -0700

    fix: Change the date and map screen data

--- src/model/mapData.js
+++ src/model/mapData.js
@@ -1,27 +1,32 @@
 export const heatMap = [
   {
-    latitude: 49.279672,
-    longitude: -123.130293,
-    weight: 5063,
-  },
-  {
-    latitude: 49.030235,
-    longitude: -122.287062,
-    weight: 5502,
-  },
-  {
-    latitude: 49.2776612,
-    longitude: -123.1081074,
-    weight: 3461,
-  },
-  {
     latitude: 49.2806462,
     longitude: -123.1208652,
-    weight: 11026,
+    weight: 4446,
   },
   {
-    latitude: 49.2776612,
-    longitude: -123.1081074,
-    weight: 11229,
+    latitude: 49.2805197,
+    longitude: -123.1007382,
+    weight: 8714,
   },
+  {
+    latitude: 49.27967,
+    longitude: -123.130293,
+    weight: 7552,
+  },
+  {
+    latitude: 49.27950169,
+    longitude: -123.1299918,
+    weight: 4608,
+  },
+  {
+    latitude: 49.2760688,
+    longitude: -123.1127117,
+    weight: 6195,
+  },
+  {
+    latitude: 49.2777469,
+    longitude: -123.1174817,
+    weight: 10669
+  }
 ]
